45d95ac417615752c19e0ba0ff38eef054606d17
ed <humdrum375@gmail.com>
2018-04-30 16:30:05 +0100

    spec

--- spec/finance_spec.cr
+++ spec/finance_spec.cr
@@ -1,14 +1,16 @@
 require "./spec_helper"
 
 describe "Payment" do
-  it "Calculates Payment" do
+  it "Calculates PMT" do
     payment_0 = Finance.payment(12995.0, 6000.0, 9.3, 36, 0)
     payment_0.should eq(269.92)
 
     payment_1 = Finance.payment(12995.0, 6000.0, 9.3, 36, 1)
     payment_1.should eq(267.84)
   end
+end
 
+describe "Present Value" do
   it "Calculates PV" do
     pv_0 = Finance.pv(120.00, 1, 100.00, 100.00, 0)
     pv_0.should eq(181.82)
